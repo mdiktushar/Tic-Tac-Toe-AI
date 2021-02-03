2a3dd3367a163617c34a6d9c3733631ca27aa474
mdiktushar <md.tushar1804@gmail.com>
2021-02-03 18:23:40 +0600

    commit-1 03/02/2021

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var f = true;
 
 
 // Random Selsction for 1st move in Pc 
-squares[Math.floor(Math.random() * 9)].textContent = 'O';
+squares[Math.floor(Math.random() * 9)].textContent = 'X';
 
 
 // Clear all the sequares
@@ -24,7 +24,7 @@ restart.addEventListener('click', function clearBoard(){
             squares[i].textContent = '';
 
     }
-    squares[Math.floor(Math.random() * 9)].textContent = 'O';
+    squares[Math.floor(Math.random() * 9)].textContent = 'X';
     res.textContent = '';
     Tag.textContent = 'Get ready to play!';
 });
@@ -61,7 +61,7 @@ function result(){
     if((squares[0].textContent !== '' && squares[1].textContent !== '' && squares[2].textContent !== '') 
     && (squares[0].textContent === squares[1].textContent && squares[1].textContent === squares[2].textContent )){
         
-        if(squares[0].textContent === 'O'){
+        if(squares[0].textContent === 'X'){
             pcWinn()
         }  
         else{
@@ -72,7 +72,7 @@ function result(){
     }else if((squares[3].textContent !== '' && squares[4].textContent !== '' && squares[5].textContent !== '') 
     && (squares[3].textContent === squares[4].textContent && squares[4].textContent === squares[5].textContent)){
 
-        if(squares[3].textContent === 'O'){
+        if(squares[3].textContent === 'X'){
             pcWinn()
         }  
         else{
@@ -82,7 +82,7 @@ function result(){
     }else if((squares[6].textContent !== '' && squares[7].textContent !== '' && squares[8].textContent !== '') 
     && (squares[6].textContent === squares[7].textContent && squares[7].textContent === squares[8].textContent)){
 
-        if(squares[6].textContent === 'O'){
+        if(squares[6].textContent === 'X'){
             pcWinn()
         }  
         else{
@@ -92,7 +92,7 @@ function result(){
     }else if((squares[0].textContent !== '' && squares[3].textContent !== '' && squares[4].textContent !== '') 
     && (squares[0].textContent === squares[3].textContent && squares[3].textContent === squares[6].textContent )){
 
-        if(squares[0].textContent === 'O'){
+        if(squares[0].textContent === 'X'){
             pcWinn()
         }  
         else{
@@ -102,7 +102,7 @@ function result(){
     }else if((squares[7].textContent !== '' && squares[4].textContent !== '' && squares[1].textContent !== '') 
     && (squares[1].textContent === squares[4].textContent && squares[4].textContent === squares[7].textContent)){
 
-        if(squares[1].textContent === 'O'){
+        if(squares[1].textContent === 'X'){
             pcWinn()
         }  
         else{
@@ -112,7 +112,7 @@ function result(){
     }else if((squares[8].textContent !== '' && squares[5].textContent !== '' && squares[2].textContent !== '') 
     && (squares[2].textContent === squares[5].textContent && squares[5].textContent === squares[8].textContent)){
 
-        if(squares[2].textContent === 'O'){
+        if(squares[2].textContent === 'X'){
             pcWinn()
         }  
         else{
@@ -122,7 +122,7 @@ function result(){
     }else if((squares[0].textContent !== '' && squares[4].textContent !== '' && squares[8].textContent !== '') 
     && (squares[0].textContent === squares[4].textContent && squares[4].textContent === squares[8].textContent)){
 
-        if(squares[0].textContent === 'O'){
+        if(squares[0].textContent === 'X'){
             pcWinn()
         }  
         else{
@@ -132,7 +132,7 @@ function result(){
     }else if((squares[2].textContent !== '' && squares[4].textContent !== '' && squares[6].textContent !== '') 
     && (squares[2].textContent === squares[4].textContent && squares[4].textContent === squares[6].textContent)){
 
-        if(squares[2].textContent === 'O'){
+        if(squares[2].textContent === 'X'){
             pcWinn()
         }  
         else{
@@ -143,9 +143,9 @@ function result(){
     else if(squares[0].textContent !== '' && squares[1].textContent !== '' && squares[2].textContent !== '' 
     && squares[3].textContent !== '' && squares[4].textContent !== '' && squares[5].textContent !== '' 
     && squares[6].textContent !== ''&& squares[7].textContent !== '' && squares[8].textContent !== ''){
-        console.log("Drow");
+        console.log("Draw");
         Tag.textContent = '';
-        res.textContent = "........................................Match Drow........................................";
+        res.textContent = "........................................Match Draw........................................";
         f = false;
     }
 
@@ -153,131 +153,131 @@ function result(){
 // ..................................................................
 
 // AI Function
-function attact(v){
+function decision(v){
     if(squares[2].textContent === squares[4].textContent && squares[2].textContent === v && squares[6].textContent === ''){
-        squares[6].textContent = 'O';
+        squares[6].textContent = 'X';
         console.log("1");
         return false;
     }
     else if(squares[6].textContent === squares[4].textContent && squares[4].textContent === v && squares[2].textContent === ''){
-        squares[2].textContent = 'O';
+        squares[2].textContent = 'X';
         console.log("2");
         return false;
     }
     else if(squares[2].textContent === squares[6].textContent && squares[2].textContent === v && squares[4].textContent === ''){
-        squares[4].textContent = 'O';
+        squares[4].textContent = 'X';
         console.log("3");
         return false;
     }
 
     else if(squares[0].textContent === squares[1].textContent && squares[1].textContent === v && squares[2].textContent === ''){
-        squares[2].textContent = 'O';
+        squares[2].textContent = 'X';
         console.log("4");
         return false;
     }
     else if(squares[3].textContent === squares[4].textContent && squares[3].textContent === v && squares[5].textContent === ''){
-        squares[5].textContent = 'O';
+        squares[5].textContent = 'X';
         console.log("5");
         return false;
     }
     else if(squares[6].textContent === squares[7].textContent && squares[7].textContent === v && squares[8].textContent === ''){
-        squares[8].textContent = 'O';
+        squares[8].textContent = 'X';
         console.log("6");
         return false;
     }
 
     else if(squares[0].textContent === squares[2].textContent && squares[0].textContent === v && squares[1].textContent === ''){
-        squares[1].textContent = 'O';
+        squares[1].textContent = 'X';
         console.log("7");
         return false;
     }
     else if(squares[3].textContent === squares[5].textContent && squares[5].textContent === v && squares[4].textContent === ''){
-        squares[4].textContent = 'O';
+        squares[4].textContent = 'X';
         console.log("8");
         return false;
     }
     else if(squares[6].textContent === squares[8].textContent && squares[8].textContent === v && squares[7].textContent === ''){
-        squares[7].textContent = 'O';
+        squares[7].textContent = 'X';
         console.log("9");
         return false;
     }
 
     else if(squares[1].textContent === squares[2].textContent && squares[1].textContent === v && squares[0].textContent === ''){
-        squares[0].textContent = 'O';
+        squares[0].textContent = 'X';
         console.log("10");
         return false;
     }
     else if(squares[4].textContent === squares[5].textContent && squares[5].textContent === v && squares[3].textContent === ''){
-        squares[3].textContent = 'O';
+        squares[3].textContent = 'X';
         console.log("11");
         return false;
     }
     else if(squares[7].textContent === squares[8].textContent && squares[8].textContent === v && squares[6].textContent === ''){
-        squares[6].textContent = 'O';
+        squares[6].textContent = 'X';
         console.log("12");
         return false;
     }
 
     else if(squares[0].textContent === squares[3].textContent && squares[0].textContent === v && squares[6].textContent === ''){
-        squares[6].textContent = 'O';
+        squares[6].textContent = 'X';
         console.log("13");
         return false;
     }
     else if(squares[1].textContent === squares[4].textContent && squares[4].textContent === v && squares[7].textContent === ''){
-        squares[7].textContent = 'O';
+        squares[7].textContent = 'X';
         console.log("14");
         return false;
     }
     else if(squares[2].textContent === squares[5].textContent && squares[5].textContent === v && squares[8].textContent === ''){
-        squares[8].textContent = 'O';
+        squares[8].textContent = 'X';
         console.log("15");
         return false;
     }
 
     else if(squares[0].textContent === squares[6].textContent && squares[0].textContent === v && squares[3].textContent === ''){
-        squares[3].textContent = 'O';
+        squares[3].textContent = 'X';
         console.log("16");
         return false;
     }
     else if(squares[1].textContent === squares[7].textContent && squares[7].textContent === v && squares[4].textContent === ''){
-        squares[4].textContent = 'O';
+        squares[4].textContent = 'X';
         console.log("17");
         return false;
     }
     else if(squares[2].textContent === squares[8].textContent && squares[8].textContent === v && squares[5].textContent === ''){
-        squares[5].textContent = 'O';
+        squares[5].textContent = 'X';
         console.log("18");
         return false;
     }
 
     else if(squares[3].textContent === squares[6].textContent && squares[3].textContent === v && squares[0].textContent === ''){
-        squares[0].textContent = 'O';
+        squares[0].textContent = 'X';
         console.log("19");
         return false;
     }
     else if(squares[4].textContent === squares[7].textContent && squares[7].textContent === v && squares[1].textContent === ''){
-        squares[1].textContent = 'O';
+        squares[1].textContent = 'X';
         console.log("20");
         return false;
     }
     else if(squares[5].textContent === squares[8].textContent && squares[8].textContent === v && squares[2].textContent === ''){
-        squares[2].textContent = 'O';
+        squares[2].textContent = 'X';
         console.log("21");
         return false;
     }
 
     else if(squares[0].textContent === squares[4].textContent && squares[0].textContent === v && squares[8].textContent === ''){
-        squares[8].textContent = 'O';
+        squares[8].textContent = 'X';
         console.log("22");
         return false;
     }
     else if(squares[0].textContent === squares[8].textContent && squares[0].textContent === v && squares[4].textContent === ''){
-        squares[4].textContent = 'O';
+        squares[4].textContent = 'X';
         console.log("23");
         return false;
     }
     else if(squares[8].textContent === squares[4].textContent && squares[8].textContent === v && squares[0].textContent === ''){
-        squares[0].textContent = 'O';
+        squares[0].textContent = 'X';
         console.log("24");
         return false;
     }
@@ -294,18 +294,18 @@ function pcMove(){
 
     f = true;
     console.log(f);
-    f = attact('O');
+    f = decision('X');
     console.log(f,"attact");
 
     if (f === true){
-        f = attact('X');
+        f = decision('O');
         console.log(f,"defence");
     }
     
     if(f === true){
         for (i=0; i<squares.length; i++){
             if(squares[i].textContent === ''){
-                squares[i].textContent = 'O';
+                squares[i].textContent = 'X';
                 break;
             }
         }
@@ -321,7 +321,7 @@ function pcMove(){
 function clickListener(){
     
     if(this.textContent === ''){
-        this.textContent = 'X';
+        this.textContent = 'O';
         result();
         pcMove();
         result();
